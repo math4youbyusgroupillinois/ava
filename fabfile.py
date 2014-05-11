eddcd1a667d36c94e98a42279db2090149eaf69b
ladynerd (Laura Bell) <laura.d.bell+projects@gmail.com>
2014-05-11 22:15:12 +1200

    Clean up and simplfy of fabric file

--- fabfile.py
+++ fabfile.py
@@ -2,13 +2,13 @@ from fabric.api import local
 from fabric.api import lcd, local
 
 def deploy():
+    local('git add -p && git commit') # or local('hg add && hg commit')
     with lcd('/home/ladynerd/deploy/ava/'):
-
-        # With git...
         local('git pull /home/ladynerd/projects/ava/')
 
 
 def prepare_deployment():
+    pass
     #local('python manage.py test ava_core')
     #local('python manage.py test ava_core_people/tests')
-    local('git add -p && git commit') # or local('hg add && hg commit')
+    #local('git add -p && git commit') 
