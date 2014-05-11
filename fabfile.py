2dbae2502ace48afdc3becbb0f448892caf09eb8
ladynerd (Laura Bell) <laura.d.bell+projects@gmail.com>
2014-05-11 22:19:54 +1200

    Additional functions in fab file

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,19 @@
 from fabric.api import local
 from fabric.api import lcd, local
 
-def deploy():
+
+def quick():
     local('git add -p && git commit') # or local('hg add && hg commit')
     with lcd('/home/ladynerd/deploy/ava/'):
         local('git pull /home/ladynerd/projects/ava/')
 
 
+def deploy():
+    with lcd('/home/ladynerd/deploy/ava/'):
+        local('git pull /home/ladynerd/projects/ava/')
+
+
 def prepare_deployment():
-    pass
     #local('python manage.py test ava_core')
     #local('python manage.py test ava_core_people/tests')
-    #local('git add -p && git commit') 
+    local('git add -p && git commit') 
