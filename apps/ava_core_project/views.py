1706c264d9b53ef224e111f0ed3524bdfd48f386
ladynerd (Laura Bell) <laura.d.bell+projects@gmail.com>
2014-05-12 20:30:09 +1200

    Cleaning logout link

--- apps/ava_core_project/views.py
+++ apps/ava_core_project/views.py
@@ -14,6 +14,7 @@ class DashboardView(generic.ListView):
     context_object_name = 'project_list'
 
     def get_queryset(self):
+        print self.request.user
         self.request.session['project']=None 
         return Project.objects.filter(user=self.request.user)
 
