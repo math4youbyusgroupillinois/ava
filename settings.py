7fad5fce34b0d667257e1b061cf305c0f8cddac9
ladynerd (Laura Bell) <laura.d.bell+projects@gmail.com>
2014-05-12 21:45:02 +1200

    Science~

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 TEMPLATE_DIRS = (
-            os.path.join(BASE_DIR,  'templates'),
+            os.path.join(BASE_DIR,  'ava.templates'),
             )
 
 DATABASES = {
