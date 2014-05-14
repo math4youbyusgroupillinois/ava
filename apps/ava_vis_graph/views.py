df56b6eac1501a96c6d930022948abbb961eec28
ladynerd (Laura Bell) <laura.d.bell+projects@gmail.com>
2014-05-14 20:21:39 +1200

    more debugging

--- apps/ava_vis_graph/views.py
+++ apps/ava_vis_graph/views.py
@@ -41,7 +41,7 @@ class ExportLDAP():
         for key,value in index.iteritems():
              if isinstance(value,ActiveDirectoryUser):
                  groups = value.memberOf
-                 #print groups
+                 print groups.all()
                  for group in groups.all():
                      print group
                      e = {}
