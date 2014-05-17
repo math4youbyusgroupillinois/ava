6a9b2df4fb6714f6f576ce094b7cb96dd10ad05f
ladynerd (Laura Bell) <laura.d.bell+projects@gmail.com>
2014-05-17 22:35:06 +1200

    k

--- apps/ava_vis_graph/urls.py
+++ apps/ava_vis_graph/urls.py
@@ -6,5 +6,5 @@ from apps.ava_vis_graph import views
 urlpatterns = patterns('',
     
     url(r'^ldap/(?P<pk>\d+)/$', login_required(views.LDAPGraphView.as_view()), name='ldap_graph_view'),
-    url(r'^ldap/(?P<pk>\d+)/hide/$', login_required(views.LDAPGraphHideView.as_view()), name='ldap_graph_view'),
+    #url(r'^ldap/(?P<pk>\d+)/hide/$', login_required(views.LDAPGraphHideView.as_view()), name='ldap_graph_view'),
 )
