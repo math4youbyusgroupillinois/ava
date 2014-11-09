05aed9d95e042f9fd4a65374166e406bdceba410
ladynerd (Laura Bell) <laura.d.bell+projects@gmail.com>
2014-11-09 22:05:39 +1300

    basics for test suite

---
+++ apps/ava_test_email/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from apps.ava_test_email.models import *
+
+admin.site.register(EmailTest)
+admin.site.register(EmailTestType)
+admin.site.register(EmailMessageType)
+
